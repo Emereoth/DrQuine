df330b3a05586d3a265c1ff602294f6d93e16959
Andras COTTIER <acottier@e1r7p8.42.fr>
2018-09-08 19:29:58 +0200

    hey, what's that over there?!

--- C/Colleen/sources/Colleen.c
+++ C/Colleen/sources/Colleen.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
-#include <unistd.h>
 
-void toast(char *str)
+/*
+** yo wassup
+*/
+
+void toast(void) {};
+
+int main(void)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			printf("\\n");
-		else if (str[i] == '\t')
-			printf("\\t");
-		else
-			printf("%c", str[i]);
-		str++;
-	}
-}
-
-// wassup
-
-int	main(void)
-{
-	// ayy lmao
-
-	char *str = "#include <stdio.h>\n#include <unistd.h>\n\nvoid toast(char * str)\n{\n\tint i;\n\n\ti = 0;\n\twhile (str[i])\n\t{\n\t\tif (str[i] == '\\n')\n\t\t\tprintf(\"\\n\");\n\t\telse if (str[i] == '\\t')\n\t\t\tprintf(\"\\t\");\n\t\telse\n\t\t\tprintf(\"%c\", str[i]);\n\t\tstr++;\n\t}\n}\n\n// wassup\n\nint main(void)\n{\n\t// ayy lmao\n\n\tchar *str = \"\";\n\n\tprintf(\"%.*s\", 287, str);\n\ttoast(str);\n\tprintf(\"%s\", str + 287);\n\n\treturn (0);\n}";
-
-	printf("%.*s", 287, str);
-	toast(str);
-	printf("%s", str + 287);
-
-	return (0);
+	/* ayy lmao */
+	char *str = "#include <stdio.h>%1$c%1$c/*%1$c** yo wassup%1$c*/%1$c%1$cvoid toast(void) {};%1$c%1$cint main(void)%1$c{%1$c%2$c/* ayy lmao */%1$c%2$cchar *str = %3$c%4$s%3$c;%1$c%2$cprintf(str, 10, 9, 34, str);%1$c}%1$c";
+	printf(str, 10, 9, 34, str);
 }
