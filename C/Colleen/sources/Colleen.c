bf62a7f6eb865bd4d5ed57577ef500bf6ed72009
Andras COTTIER <acottier@e1r4p12.42.fr>
2018-09-17 18:05:36 +0200

    foo

--- C/Colleen/sources/Colleen.c
+++ C/Colleen/sources/Colleen.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-** yo wassup
+ yo wassup
 */
 
 void toast(void) {};
@@ -9,6 +9,6 @@ void toast(void) {};
 int main(void)
 {
 	/* ayy lmao */
-	char *str = "#include <stdio.h>%1$c%1$c/*%1$c** yo wassup%1$c*/%1$c%1$cvoid toast(void) {};%1$c%1$cint main(void)%1$c{%1$c%2$c/* ayy lmao */%1$c%2$cchar *str = %3$c%4$s%3$c;%1$c%2$cprintf(str, 10, 9, 34, str);%1$c}%1$c";
+	char *str = "#include <stdio.h>%1$c%1$c/*%1$c yo wassup%1$c*/%1$c%1$cvoid toast(void) {};%1$c%1$cint main(void)%1$c{%1$c%2$c/* ayy lmao */%1$c%2$cchar *str = %3$c%4$s%3$c;%1$c%2$cprintf(str, 10, 9, 34, str);%1$c}%1$c";
 	printf(str, 10, 9, 34, str);
 }
